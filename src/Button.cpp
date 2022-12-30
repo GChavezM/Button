8424259023c6aca2d1ea0e7939ec6ead0e434916
Gabriel Chavez <gchavezmardonez@gmail.com>
2022-12-29 23:51:40 -0400

    Button

---
+++ src/Button.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include "Button.h"
+
+Button::Button(uint8_t pin, types type, boolean pullup) {
+  _pin = pin;
+  _block_time = 0;
+  _type = type;
+  _last_option = NONE;
+  if (_type == SIMPLE) {
+    _invert = false;
+    if (pullup) {
+      pinMode(_pin, INPUT_PULLUP);
+      _invert = true;      
+    } else {
+      pinMode(_pin, INPUT);
+    }
+    _last_state = getRawValue();
+  } 
+}
+
+int Button::getRawValue() {
+  if (_type == ANALOG) {
+    return analogRead(_pin);
+  }
+  return _invert ? !digitalRead(_pin) : digitalRead(_pin);
+}
+
+options Button::getRawOption() {
+  if (_type == ANALOG) {
+    int rawValue = getRawValue();
+    if (rawValue > OPTION_TRESHOLD_NONE) return NONE;
+    if (rawValue < OPTION_TRESHOLD_RIGHT) return RIGHT;
+    if (rawValue < OPTION_TRESHOLD_UP) return UP;
+    if (rawValue < OPTION_TRESHOLD_DOWN) return DOWN;
+    if (rawValue < OPTION_TRESHOLD_LEFT) return LEFT;
+    if (rawValue < OPTION_TRESHOLD_SELECT) return SELECT;
+    return NONE;
+  }
+  return NONE;
+}
+
+String Button::getStringValue() {
+  options option = getRawOption();
+  if (option == RIGHT) return "RIGHT";
+  if (option == UP) return "UP";
+  if (option == DOWN) return "DOWN";
+  if (option == LEFT) return "LEFT";
+  if (option == SELECT) return "SELECT";
+  return "";
+}
+
+options Button::getOption(uint16_t block_delay, uint16_t repeat_delay) {
+  if (millis() < _block_time) return NONE;
+  options current = getRawOption();
+  if (current != NONE) {
+    _block_time = millis() + (current == _last_option ? repeat_delay : block_delay);
+  }
+  _last_option = current;
+  return current;
+}
+
+int Button::getStatus(uint16_t block_delay, uint16_t repeat_delay) {
+  if (millis() < _block_time) return NONE;
+  int current = getRawValue();
+  if (current != NONE) {
+    _block_time = millis() + (current == _last_state ? repeat_delay : block_delay);
+  }
+  _last_state = current;
+  return current;
+}
---
+++ src/Button.h
@@ -0,0 +1,49 @@
+#ifndef Button_h
+#define Button_h
+
+#include <Arduino.h>
+#include <inttypes.h>
+
+enum types {
+  SIMPLE,
+  ANALOG
+};
+
+enum options {
+  NONE,
+  RIGHT,
+  UP,
+  DOWN,
+  LEFT,
+  SELECT
+};
+
+#define OPTION_TRESHOLD_NONE    1000
+#define OPTION_TRESHOLD_RIGHT   60
+#define OPTION_TRESHOLD_UP      200
+#define OPTION_TRESHOLD_DOWN    400
+#define OPTION_TRESHOLD_LEFT    600
+#define OPTION_TRESHOLD_SELECT  800
+
+class Button {
+  public:
+    Button(uint8_t pin, types type = SIMPLE, boolean pullup = true);
+
+    int getRawValue();
+    options getRawOption();
+    String getStringValue();
+    options getOption(uint16_t block_delay = 500,  uint16_t repeat_delay = 300);
+    int getStatus(uint16_t block_delay = 500, uint16_t repeat_delay = 300);
+
+  private:
+    uint8_t _pin;
+    uint32_t _block_time;
+    types _type;
+    options _last_option;
+    boolean _invert;
+    int _last_state;
+
+
+};
+
+#endif
